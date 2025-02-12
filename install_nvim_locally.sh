548859222de2933df1e08619962a3050d5a9c1ad
Gustavo Moura <88595534+gustavomoura628@users.noreply.github.com>
2025-02-12 14:36:49 -0300

    Update install_nvim_locally.sh

--- install_nvim_locally.sh
+++ install_nvim_locally.sh
@@ -3,6 +3,16 @@ if command -v nvim; then echo nvim is already installed!; exit 0; fi
 
 mkdir /tmp/install_nvim_locally
 cd /tmp/install_nvim_locally
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-cp nvim-linux64/bin/nvim ~/.local/bin
+
+# Broken
+#wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+
+FILE_NAME=nvim-linux-$(uname -m)
+
+wget "https://github.com/neovim/neovim/releases/download/nightly/$FILE_NAME.tar.gz"
+tar xzvf $FILE_NAME.tar.gz
+
+mkdir -p ~/.local/bin
+cp $FILE_NAME/bin/nvim ~/.local/bin
+
+
